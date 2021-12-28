c5a8312f1a03e336e0e878ec4fc6669edf1dbb11
Christopher Hauser <chauser6@gmail.com>
2021-12-27 19:01:33 -0500

    done

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,17 +1,15 @@
 def parse(value):
-    if value == "I":
-        return 1
-    elif value == "II":
-        return 2
-    elif value == "III":
-        return 3
-    elif value == "IV":
-        return 4
-    elif value == "V":
-        return 5
-    elif value == "VI":
-        return 6
-    elif value == "VII":
-        return 7
-    elif value == "VIII":
-        return 8
+
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+
+    i = 0
+    num = 0
+    while i < len(value):
+        if (i + 1 < len(value) and value[i: i + 2] in roman_values):
+            num += roman_values[value[i: i + 2]]
+            i += 2
+        else:
+            num += roman_values[value[i]]
+            i += 1
+
+    return num
